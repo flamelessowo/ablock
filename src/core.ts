3eda06e02d76dc6326809e71326d5c1692a45e43
Christian <flamelessowo@gmail.com>
2024-05-22 16:51:16 +0300

    Cleanup

--- src/core.ts
+++ src/core.ts
@@ -1,48 +1,29 @@
-export const russian_propaganda_keywords = [
-  "Kremlin",
-  "Putin",
-  "Russia",
-  "Russian",
-  "moscow",
-  "Soviet",
-  "USSR",
-  "KGB",
-  "FSB",
-  "GRU",
-  "Oligarch",
-  "DNR",
-  "LNR",
-  "Donbass",
-  "Novorossiya",
-  "Crimea",
-]
+import { levenshtein, russian_propaganda_keywords } from "./levenstein";
 
-export function levenshtein(a: string, b: string): number {
-  const an = a ? a.length : 0;
-  const bn = b ? b.length : 0;
-  if (an === 0) {
-    return bn;
+const MutatorCallbacks = {
+  TWITTER_ARTICLE: prepareMutatorCallback('article'),
+  TWITTER_COMMENTS: prepareMutatorCallback('div[dir="auto"] span[style="text-overflow: unset;"]'),
+  YOUTUBE_VIDEO: prepareMutatorCallback('ytd-rich-item-renderer'),
+  YOUTUBE_COMMENTS: prepareMutatorCallback('#content-text > span'),
+  INSTAGRAM_ARTICLE: prepareMutatorCallback('article'),
+  INSTAGRAM_COMMENTS: prepareMutatorCallback('span[dir="auto"]'),
+};
+
+function prepareMutatorCallback(nodeSelector: string) {
+  return (mutations: MutationRecord[]) => {
+    mutations.forEach(mutation => {
+      mutation.addedNodes.forEach(node => {
+        if (node.nodeType === Node.ELEMENT_NODE) {
+          const element = node as Element;
+          element.querySelectorAll(nodeSelector).forEach(selectedNode => {
+            console.log('wtf')
+            selectedNode.innerHTML = 'Blocked'; // ADD BLOCK HANDLER
+          });
+        }
+      });
+    });
   }
-  if (bn === 0) {
-    return an;
-  }
-  const matrix = Array.from({ length: an + 1 }, (_, i) => [i]);
-  for (let j = 1; j <= bn; j++) {
-    matrix[0][j] = j;
-  }
-  for (let i = 1; i <= an; i++) {
-    for (let j = 1; j <= bn; j++) {
-      const cost = a[i - 1] === b[j - 1] ? 0 : 1;
-      matrix[i][j] = Math.min(
-        matrix[i - 1][j] + 1, // Deletion
-        matrix[i][j - 1] + 1, // Insertion
-        matrix[i - 1][j - 1] + cost // Substitution
-      );
-    }
-  }
-  return matrix[an][bn];
 }
-
 
 enum PageType {
   YOUTUBE = 'www.youtube.com',
@@ -62,13 +43,18 @@ interface Application {
 }
 
 abstract class Adapter {
-  abstract selectors: AdapterSelectors;
   abstract prepareObserver(): MutationObserver;
   observer: MutationObserver;
-  abstract run(): void;
 
   constructor() {
     this.observer = this.prepareObserver();
+  }
+
+  run() {
+    this.observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    });
   }
 
   containsSimilarKeyword(text: string, keywords: string[] = russian_propaganda_keywords, threshold: number = 2): boolean {
@@ -81,100 +67,43 @@ abstract class Adapter {
   }
 }
 
-interface AdapterSelectors {
-  article: string | null;
-  articleComment: string | null;
-  videoImage: string | null;
-  videoComment: string | null;
-}
 
 class YoutubeAdapter extends Adapter {
-  selectors: AdapterSelectors;
 
   constructor() {
     super();
-    this.selectors = {
-      article: 'ytd-app',
-      articleComment: 'ytd-comment-thread-renderer',
-      videoImage: 'ytd-thumbnail',
-      videoComment: 'ytd-comment-renderer',
-    };
   }
 
   prepareObserver(): MutationObserver {
-    const observer = new MutationObserver(mutations => {
-      mutations.forEach(mutation => {
-        mutation.addedNodes.forEach((node) => {
-          if (node.nodeType === Node.ELEMENT_NODE) {
-            const element = node as Element;
-            if (element.matches('yt-formatted-string')) {
-              element.innerHTML = 'Pig'
-            }
-          }
-        });
-      });
-    });
-    return observer;
-  }
-
-  run() {
-    this.observer.observe(document.body, {
-      childList: true,
-      subtree: true,
-    });
+    if (window.location.pathname.includes('/watch')) {
+      return new MutationObserver(MutatorCallbacks['YOUTUBE_COMMENTS']);
+    }
+    return new MutationObserver(MutatorCallbacks['YOUTUBE_VIDEO']);
   }
 }
 
 class TwitterAdapter extends Adapter {
-  selectors: AdapterSelectors;
 
   constructor() {
     super();
-    this.selectors = {
-      article: 'article',
-      articleComment: 'article',
-      videoImage: 'img',
-      videoComment: 'article',
-    };
   }
 
   prepareObserver(): MutationObserver {
-    const observer = new MutationObserver(mutations => {
-      mutations.forEach(mutation => {
-        // Your logic for handling mutations
-      });
-    });
-    return observer;
-  }
-
-  run() {
-
+    if (window.location.pathname.includes('/status/')) {
+      return new MutationObserver(MutatorCallbacks['TWITTER_COMMENTS'])
+    }
+    return new MutationObserver(MutatorCallbacks['TWITTER_ARTICLE'])
   }
 }
 
 class InstagramAdapter extends Adapter {
-  selectors: AdapterSelectors;
 
   constructor() {
     super();
-    this.selectors = {
-      article: 'article',
-      articleComment: 'article',
-      videoImage: 'img',
-      videoComment: 'article',
-    };
   }
 
   prepareObserver(): MutationObserver {
-    const observer = new MutationObserver(mutations => {
-      mutations.forEach(mutation => {
-        // Your logic for handling mutations
-      });
-    });
-    return observer;
-  }
-  run() {
-
+    return new MutationObserver(MutatorCallbacks['INSTAGRAM_COMMENTS'])
   }
 }
 
@@ -200,6 +129,9 @@ export class AblockApp implements Application {
     }
     if (url.includes(PageType.TWITTER)) {
       return new TwitterAdapter();
+    }
+    if (url.includes(PageType.INSTAGRAM)) {
+      return new InstagramAdapter();
     }
     return new InstagramAdapter();
   }
